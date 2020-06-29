d53a7d870c2ec5173d580904271d851ba271ef7f
otsubo-jyobi <otsubo@morijyobi.ac.jp>
2020-06-29 11:59:33 +0900

    add delete

--- delete.php
+++ delete.php
@@ -6,13 +6,11 @@ try {
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $age = $_POST['age'];
+    $id = $_GET['id'];
 
-    $sql = "insert into user values (:id, :name, :age)";
+    $sql = "delete form user where id=:id";
     $stmt = $dbh->prepare($sql);
-    $prams = array(':id'=> $id, ':name'=> $name, ':age'=> $age,);
+    $prams = array(':id'=> $id);
     $stmt->execute($prams);
 
     header('Location: index.php?fg=1');
